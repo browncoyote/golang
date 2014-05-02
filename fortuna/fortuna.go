ddfe0395a85e02fee2fcdc744f04be0cb0ed25f9
browncoyote <browncoyote30@gmail.com>
2014-05-02 13:41:23 -0700

    May fix 1.0

--- fortuna/fortuna.go
+++ fortuna/fortuna.go
@@ -1,3 +1,4 @@
+//Emulate Magic 8-ball
 package main
 
 import (
@@ -5,18 +6,15 @@ import (
 	"time"
 	"math/rand"
 )
-	
-	var n int
-    
+	var n = 20
+	var q string
 func main() {
+Ask:
 	rand.Seed(time.Now().UnixNano())
+	r := rand.Intn(n)
+	answer := []string{"It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"}	
 	fmt.Println("Ask Fortuna a question.")
-	lens("Hello", "Goodbye", "yes", "no")
-	roll()
-}
-
-func roll() {
-	for i := 0; i < n; i++ {
-		fmt.Println(lens(rand.Intn(4)))
-	}
+	fmt.Scanln(&q)
+	fmt.Println(answer[r])
+	goto Ask
 }
